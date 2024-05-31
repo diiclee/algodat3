db61229b0b055d753b9782f9a4feadd2b4c53cdf
Dicle <dicle.bbr@gmail.com>
2024-05-31 12:08:29 +0200

    euro und minuten entfernt

--- main.py
+++ main.py
@@ -82,10 +82,9 @@ def find_path(filename_graph, start, end):
                     if current_line is not None:
                         path_details.append(f"Umsteigen von {current_line} zu {line} bei {original_names[path[i]]}")
                     current_line = line
-                path_details.append(f"{original_names[path[i]]} zu {original_names[path[i + 1]]} über {current_line} (Kosten: {c} Euro)")
+                path_details.append(f"{original_names[path[i]]} zu {original_names[path[i + 1]]} über {current_line} (Kosten: {c})")
                 break
-    path_details.append(f"Gesamtkosten: {total_cost} Euro")
-    path_details.append(f"Gesamtfahrzeit: {total_cost} Minuten")
+    path_details.append(f"Gesamtkosten: {total_cost}")
     return path_details
 
 if __name__ == "__main__":
